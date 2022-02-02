fa7470e9d36ab6a1b1c37de54d9dcc4b44c1ebb9
naveen <naveensrc7@gmail.com>
2022-02-01 23:42:13 -0600

    commit first git commit

---
+++ src/main/java/hhh/nd/libs/Demo.java
@@ -0,0 +1,10 @@
+package hhh.nd.libs;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi naveen");
+	}
+
+}
---
+++ src/main/java/hhh/nd/util/Page.java
@@ -0,0 +1,10 @@
+package hhh.nd.util;
+
+public class Page {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("oops");
+	}
+
+}
